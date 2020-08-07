ce4dd3fe2dddef6e88464a99b97440e037db8ec7
Daniel Krogh <Daniel Krogh@DKA>
2020-08-07 08:11:47 +0200

    .

--- js/stickyFooter.js
+++ js/stickyFooter.js
@@ -1,3 +1,4 @@
+// Footer højde sættes på body som margin-bottom 
 function setBodyMarginBottom () {
     let footer = document.querySelector('footer').clientHeight;
     let body = document.querySelector('body');
